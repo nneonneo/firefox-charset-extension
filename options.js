5ec4c3dcdf6d2672d6cda20bf264a17fd6479949
Robert Xiao <brx@cs.ubc.ca>
2024-08-05 11:59:49 +0800

    Add a custom theme.
    Inspired by tim-we/tabs-aside: https://github.com/tim-we/tabs-aside/blob/f552b4bab59d7abd01fd0e87f01faaf5f196c5c6/src/img/browserAction/context.svg?short_path=483ac6b
    
    Fixes #2.

--- options.js
+++ options.js
@@ -1,10 +1,20 @@
 const checkAutoReload = document.getElementById("auto-reload");
 const radiosIconTheme = document.querySelectorAll('input[name="icon-theme"]');
 
+function setCustomWarningVisibility(iconTheme) {
+  const customWarning = document.getElementById("custom-warning");
+  if(iconTheme === "custom") {
+    customWarning.style.display = "block";
+  } else {
+    customWarning.style.display = "none";
+  }
+}
+
 async function initSettings() {
   checkAutoReload.checked = await getAutoreloadSetting();
   let iconTheme = await getIconThemeSetting();
   document.getElementById("icon-theme-" + iconTheme).checked = true;
+  setCustomWarningVisibility(iconTheme);
 }
 
 initSettings();
@@ -13,7 +23,9 @@ checkAutoReload.addEventListener("change", async (e) => {
 });
 for(let el of radiosIconTheme) {
   el.addEventListener("change", async (e) => {
+    console.log(e);
     await setIconThemeSetting(e.target.value);
+    setCustomWarningVisibility(e.target.value);
     await refreshIconTheme();
   });
 }
